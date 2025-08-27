7eb8f4699e51282689b30d9f7aa587f7f79449d2
Hoang Anh <181938134+nhhaanh2312@users.noreply.github.com>
2025-08-28 02:30:29 +0700

    Update deepdrug.py

--- deepdrug.py
+++ deepdrug.py
@@ -3,10 +3,9 @@ import warnings
 import numpy as np
 import yaml
 import pandas as pd
-import yaml
 warnings.filterwarnings('ignore')
 import argparse
-import torch as t 
+import torch as t
 from torch import Tensor
 from pytorch_lightning import callbacks as pl_callbacks
 from pytorch_lightning import Trainer
@@ -16,23 +15,9 @@ from dataset import DeepDrug_Dataset
 from utils import * 
 
 def get_parser(parser=None):
-    if parser == None:
+    if parser is None:
         parser = argparse.ArgumentParser()
-    # parser.add_argument("-t", "--task", type=str, default='',help="task type.")
-    # parser.add_argument("-d", "--dataset", type=str, default='', help="Dataset file path.")
-    # parser.add_argument("-cv", "--cv_fold", type=int, default=0, help="cross validation fold.")
-    # parser.add_argument('-sf','--save_folder',type=str,default='')
-    # parser.add_argument('-g','--gpus',nargs='+',type=int,default=[0,])
-    # parser.add_argument('-bs','--batch_size',type=int,default=256)
-    # parser.add_argument('-e1_file','--entry1_file',type=str,default='')
-    # parser.add_argument('-e2_file','--entry2_file',type=str,default='')
-    # parser.add_argument('-e2_seq_file','--entry2_seq_file',type=str,default='None')
-    # parser.add_argument('-e1_seq_file','--entry1_seq_file',type=str,default='None')
-    # parser.add_argument('-p_file','--pair_file',type=str,default='')
-    # parser.add_argument('-l_file','--label_file',type=str,default='')
-    # parser.add_argument('-cv_file','--cv_file',type=str,default='')
-    # parser.add_argument('-v','--version',type=str,default='')
-    parser.add_argument('-cf','--configfile',type=str,default='')
+    parser.add_argument('-cf', '--configfile', type=str, default='')
     return parser
 
 
@@ -44,123 +29,139 @@ if __name__ == '__main__':
 
     configfile = args.configfile
     with open(configfile, 'r') as f:
-        config = yaml.load(f)
+        # dùng safe_load để tương thích PyYAML mới
+        config = yaml.safe_load(f)
         print(config)
     args = Struct(**config)
-    
 
     entry1_data_folder = '/'.join(args.entry1_file.split('/')[:-2])
     entry2_data_folder = '/'.join(args.entry2_file.split('/')[:-2])
     entry2_seq_file = args.entry2_seq_file
     entry1_seq_file = args.entry1_seq_file
-    assert os.path.exists(entry1_seq_file),'file does not exist: %s.'%entry1_seq_file 
-    assert os.path.exists(entry2_seq_file),'file does not exist: %s.'%entry2_seq_file
+    assert os.path.exists(entry1_seq_file), 'file does not exist: %s.' % entry1_seq_file
+    assert os.path.exists(entry2_seq_file), 'file does not exist: %s.' % entry2_seq_file
     entry_pairs_file = args.pair_file
-    pair_labels_file = args.label_file 
-    save_folder = args.save_folder 
+    pair_labels_file = args.label_file
+    save_folder = args.save_folder
     dataset = args.dataset
-    save_model_folder = pathjoin(save_folder,'models')
-    y_true_file = pathjoin(save_folder,'test_true.csv')
-    y_pred_file = pathjoin(save_folder,'test_pred.csv')
-    os.makedirs(save_folder,exist_ok=True)
-    os.makedirs(pathjoin(save_folder,'plots'),exist_ok=True)
+    save_model_folder = pathjoin(save_folder, 'models')
+    y_true_file = pathjoin(save_folder, 'test_true.csv')
+    y_pred_file = pathjoin(save_folder, 'test_pred.csv')
+    os.makedirs(save_folder, exist_ok=True)
+    os.makedirs(pathjoin(save_folder, 'plots'), exist_ok=True)
     task_type = args.task
-    dataset = args.dataset 
-    gpus = args.gpus 
-    category  = args.category 
+    dataset = args.dataset
+    gpus = args.gpus
+    category = args.category
     num_out_dim = args.num_out_dim
-    
-
-
 
     y_transfrom_func = None
-    if (dataset in ['DAVIS','BindingDB']) and (task_type =='regression') :
-            y_transfrom_func = y_log10_transfrom_func
+    if (dataset in ['DAVIS', 'BindingDB']) and (task_type == 'regression'):
+        y_transfrom_func = y_log10_transfrom_func
 
-    if args.task in ['binary','multiclass','multilabel']:
+    if args.task in ['binary', 'multiclass', 'multilabel']:
         scheduler_ReduceLROnPlateau_tracking = 'F1'
         earlystopping_tracking = "val_epoch_F1"
     else:
-        earlystopping_tracking='val_loss'
-        scheduler_ReduceLROnPlateau_tracking= 'mse'
+        earlystopping_tracking = 'val_loss'
+        scheduler_ReduceLROnPlateau_tracking = 'mse'
 
-    kwargs_dict = dict(save_folder=save_folder,task_type=task_type,
-            gpus=gpus,
-            entry1_data_folder=entry1_data_folder,
-            entry2_data_folder=entry2_data_folder,entry_pairs_file=entry_pairs_file,
-            pair_labels_file=pair_labels_file,
-            entry1_seq_file =entry1_seq_file ,entry2_seq_file = entry2_seq_file,
-            y_true_file=y_true_file,y_pred_file=y_pred_file,
-            y_transfrom_func=y_transfrom_func,
-            earlystopping_tracking=earlystopping_tracking,
-            scheduler_ReduceLROnPlateau_tracking=scheduler_ReduceLROnPlateau_tracking,    
-            )
-            
+    kwargs_dict = dict(
+        save_folder=save_folder,
+        task_type=task_type,
+        gpus=gpus,
+        entry1_data_folder=entry1_data_folder,
+        entry2_data_folder=entry2_data_folder, entry_pairs_file=entry_pairs_file,
+        pair_labels_file=pair_labels_file,
+        entry1_seq_file=entry1_seq_file, entry2_seq_file=entry2_seq_file,
+        y_true_file=y_true_file, y_pred_file=y_pred_file,
+        y_transfrom_func=y_transfrom_func,
+        earlystopping_tracking=earlystopping_tracking,
+        scheduler_ReduceLROnPlateau_tracking=scheduler_ReduceLROnPlateau_tracking,
+    )
 
-
-    ######  for original training ##########
     _ = print_args(**kwargs_dict)
 
-    datamodule = DeepDrug_Dataset(entry1_data_folder,entry2_data_folder,entry_pairs_file,
-                        pair_labels_file,
-                        task_type = task_type,
-                        y_transfrom_func=y_transfrom_func,
-                        entry2_seq_file = entry2_seq_file,
-                        entry1_seq_file = entry1_seq_file,
-                        category=category,
-                        )
+    datamodule = DeepDrug_Dataset(
+        entry1_data_folder, entry2_data_folder, entry_pairs_file,
+        pair_labels_file,
+        task_type=task_type,
+        y_transfrom_func=y_transfrom_func,
+        entry2_seq_file=entry2_seq_file,
+        entry1_seq_file=entry1_seq_file,
+        category=category,
+    )
 
-    model =  DeepDrug_Container(
-                            task_type = task_type,category=category,
-                            scheduler_ReduceLROnPlateau_tracking=scheduler_ReduceLROnPlateau_tracking,
-                            num_out_dim = num_out_dim,
-                            )
+    model = DeepDrug_Container(
+        task_type=task_type, category=category,
+        scheduler_ReduceLROnPlateau_tracking=scheduler_ReduceLROnPlateau_tracking,
+        num_out_dim=num_out_dim,
+    )
 
-    if earlystopping_tracking in ['val_loss',]:
-        earlystopping_tracking = earlystopping_tracking
+    # Thiết lập EarlyStopping/Checkpoint theo Lightning 2.x
+    if earlystopping_tracking in ['val_loss']:
         earlystopping_mode = 'min'
-        earlystopping_min_delta = 0.0001
-    elif earlystopping_tracking in ['val_epoch_F1','val_epoch_auPRC']:
+        earlystopping_min_delta = 1e-4
+    elif earlystopping_tracking in ['val_epoch_F1', 'val_epoch_auPRC']:
         earlystopping_mode = 'max'
-        earlystopping_min_delta = 0.001
+        earlystopping_min_delta = 1e-3
     else:
-        raise 
-    checkpoint_callback = pl_callbacks.ModelCheckpoint(dirpath=save_model_folder,
-                                        mode = earlystopping_mode,
-                                        monitor=earlystopping_tracking,
-                                        save_top_k=1,save_last=True,)
-    earlystop_callback = pl_callbacks.EarlyStopping(earlystopping_tracking,verbose=True,
-                                        mode = earlystopping_mode,
-                                        min_delta=earlystopping_min_delta,
-                                        patience=10,)
-    
-    trainer = Trainer(
-                    gpus=[gpus,],
-                    accelerator=None,
-                    max_epochs=200, min_epochs=5,
-                    default_root_dir= save_folder,
-                    fast_dev_run=False,
-                    check_val_every_n_epoch=1,
-                    callbacks=  [checkpoint_callback,
-                                earlystop_callback,],
-                    )
-    trainer.fit(model, datamodule=datamodule,)
+        raise ValueError(f"Unknown monitor: {earlystopping_tracking}")
 
+    checkpoint_callback = pl_callbacks.ModelCheckpoint(
+        dirpath=save_model_folder,
+        filename='{epoch}-{'+earlystopping_tracking+':.4f}',
+        monitor=earlystopping_tracking,
+        mode=earlystopping_mode,
+        save_top_k=1,
+        save_last=True,
+    )
+
+    earlystop_callback = pl_callbacks.EarlyStopping(
+        monitor=earlystopping_tracking,
+        verbose=True,
+        mode=earlystopping_mode,
+        min_delta=earlystopping_min_delta,
+        patience=10,
+    )
+
+    # Lightning 2.x: dùng accelerator/devices thay cho gpus
+    use_gpu = t.cuda.is_available()
+    trainer = Trainer(
+        accelerator="gpu" if use_gpu else "cpu",
+        devices=1 if use_gpu else "auto",
+        max_epochs=200,
+        min_epochs=5,
+        default_root_dir=save_folder,
+        fast_dev_run=False,
+        check_val_every_n_epoch=1,
+        enable_checkpointing=True,
+        callbacks=[checkpoint_callback, earlystop_callback],
+        # logger: giữ mặc định của Lightning; thêm logger nếu bạn có
+    )
+
+    trainer.fit(model, datamodule=datamodule)
 
     ################  Prediction ##################
-    print('loading best weight in %s ...'%(checkpoint_callback.best_model_path))
-    model = model.load_from_checkpoint(checkpoint_callback.best_model_path,verbose=True)
-    model.eval()    
-    
-    trainer.test(model,datamodule=datamodule,)
-    y_pred = trainer.predict(model,dataloaders =datamodule.test_dataloader())
+    print(f'loading best weight in {checkpoint_callback.best_model_path} ...')
+    # Lightning 2.x: load_from_checkpoint là @classmethod
+    model = model.__class__.load_from_checkpoint(checkpoint_callback.best_model_path)
+    model.eval()
+
+    trainer.test(model, datamodule=datamodule)
+
+    y_pred_batches = trainer.predict(model, dataloaders=datamodule.test_dataloader())
     y_true = np.array(datamodule.pair_labels[datamodule.test_indexs])
 
-    if isinstance(y_pred[0],t.Tensor):
-        y_pred = [x.cpu().data.numpy() for x in y_pred]
-    if isinstance(y_pred,t.Tensor):
-        y_pred = y_pred.cpu().data.numpy()
-    y_pred = np.concatenate(y_pred,axis=0) 
-    pd.DataFrame(y_pred).to_csv(y_pred_file,header=True,index=False)
-    pd.DataFrame(y_true).to_csv(y_true_file,header=True,index=False)
+    # gộp batch predict
+    def to_np(x):
+        if isinstance(x, t.Tensor):
+            return x.detach().cpu().numpy()
+        return x
+
+    y_pred_batches = [to_np(batch) for batch in y_pred_batches]
+    y_pred = np.concatenate(y_pred_batches, axis=0)
+
+    pd.DataFrame(y_pred).to_csv(y_pred_file, header=True, index=False)
+    pd.DataFrame(y_true).to_csv(y_true_file, header=True, index=False)
     print('save prediction completed.')
